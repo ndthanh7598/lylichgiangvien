dded14e6df4695a033a90f20b51cbf5685604759
Nguyen Quan <quannguyen.98tb@gmail.com>
2019-10-28 22:11:36 +0700

    init branch

--- LyLichGiangVien/Model/ConnectDatabase.cs
+++ LyLichGiangVien/Model/ConnectDatabase.cs
@@ -8,7 +8,7 @@ namespace LyLichGiangVien.Model
 {
     public class ConnectDatabase
     {
-        public string strCon = @"Data Source=DESKTOP-KLCUD63\THANH;Initial Catalog=lylichgiangvien;Integrated Security=True";
+        public string strCon = @"Data Source=DESKTOP-LCKS2LS\Q;Initial Catalog=Fithou.Web.Adv;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         public SqlConnection connectDatabase()
         {
             SqlConnection myCnn = new SqlConnection(strCon);
